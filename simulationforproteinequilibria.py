24902c499a2146ebbcdf7c2136f595e14db3d9ed
ddrathod-create <ddrathod@asu.edu>
2026-07-02 13:26:14 -0700

    Update simulationforproteinequilibria.py

--- simulationforproteinequilibria.py
+++ simulationforproteinequilibria.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 st.set_page_config(
     page_title="FCS-Protein Oligomerization Simulator",
-    page_icon="confocal-vol.png",
+    page_icon="icons/confocal-vol.png",
     layout="wide",
 )
 
